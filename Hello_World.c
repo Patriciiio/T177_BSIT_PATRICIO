d2454bfcd7d0bb81c8b450f3a3afcabaa6205c3d
Patriciiio <115531213+Patriciiio@users.noreply.github.com>
2022-10-11 17:32:36 +0800

    Add files via upload

---
+++ Hello_World.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+int num1= 2000;
+
+float num2= 34.5;
+
+
+    printf("Senen Jeremiah L. Patricio\nI am 19 years old\n the value of num1 is %d, and num2 is %f, num1, num2");
+
+    return 0;
+
+}
+
+
